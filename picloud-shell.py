7c39f5fdfff0d93b00875e1811e470dac38b3357
Zach Dwiel <zdwiel@plotwatt.com>
2011-06-21 22:10:21 +0000

    first real commit

---
+++ picloud-shell.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import cloud
+import os
+cloud.setkey(os.environ['PICLOUD_KEY'], os.environ['PICLOUD_SECRET'])
+
+def sys(cmd) :
+  from subprocess import Popen, PIPE
+  return Popen(cmd, stdout=PIPE, shell=True).communicate()[0]
+
+def cloudsys(cmd) :
+  print cloud.result(cloud.call(sys, cmd))
+
+from sys import stdin
+while True :
+  print "$ ",
+  line = stdin.readline()
+  cloudsys(line)
